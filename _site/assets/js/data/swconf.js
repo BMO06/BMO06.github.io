66b9ea5ef8c13c34cac2e26b8213da20bba8758e
김환범 <han@Hans-MacBook-Pro.local>
2024-03-03 16:36:30 +0900

    uuu

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1709450891',resources: [
+    cacheName: 'chirpy-1709451335',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
